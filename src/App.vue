6b91e0b0c6313ba4d2ab15828e1d7014f07916bf
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-03-13 18:11:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import header from './components/header.vue'
+
+export default{
+  components: {
+      header,
+    },
+    data() {
+      return {
+        
+      }
+    },
+}
+</script>
+
+<template>
+  <Header/>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
